d54ab4940a0c3545ddf64f00b2f306f39c880c6c
bachupranitha <bachupranitha@gmail.com>
2026-01-19 19:47:41 +0530

    Delete The-ReBookHub-main/frontend directory

--- The-ReBookHub-main/frontend/src/contextprovider.js
+++
@@ -1,7 +0,0 @@
-// src/UserContext.js
-import { createContext, useContext } from "react";
-
-export const UserContext = createContext(null);
-
-// Custom hook for easier use
-export const useUser = () => useContext(UserContext);
